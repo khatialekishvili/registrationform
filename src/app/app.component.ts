85d663f5d0dc12e5711bf72e4d84d52f05e2a324
khatialekishvili <xatialekishvili12345@gmail.com>
2025-01-16 00:30:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [ReactiveFormsModule, CommonModule]  
 })
-export class AppComponent {
-  title = 'registrationform';
+export class AppComponent implements OnInit {
+  registrationForm!: FormGroup; 
+  title: any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      address: [''],
+      city: ['', [Validators.required]],
+      zipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]]
+    }, {
+      validators: this.passwordMatchValidator 
+    });
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };  
+  }
+
+  onSubmit() {
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    console.log('Form submitted successfully', this.registrationForm.value);
+  }
 }
